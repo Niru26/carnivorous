8aa4c46f8cdbfbcc77fac4ae4bd5b8a9beab2582
Nikolay <nikolayrudakov@mail.ru>
2023-04-01 16:19:36 +0600

    Finished task 1(1st prey) in homework 3

--- Compsognathus/1st_prey.py
+++ Compsognathus/1st_prey.py
@@ -5,6 +5,22 @@ import os
 import random
 
 os.system('cls')
-array_size = int(input())
+array_size = int(input("Enter array size: "))
 tested_array = [random.randint(1, 5) for _ in range(array_size)]
 print(tested_array)
+
+
+def find_index():
+    searched_number = int(input("Enter searched value: "))
+    counter = 0
+    index_list = ''
+    index = 0
+    for i in tested_array:
+        if i == searched_number:
+            counter += 1
+            index_list += str(index) + ' '
+        index += 1
+    print(f"We found {counter} elements, at {index_list}indexes")
+
+
+find_index()
