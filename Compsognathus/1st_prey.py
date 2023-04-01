8416402e4f934e605be6c3229ac01b80a8e8d87b
Nikolay <nikolayrudakov@mail.ru>
2023-04-01 13:46:50 +0600

    Start work on Homework 3, task 1

---
+++ Compsognathus/1st_prey.py
@@ -0,0 +1,10 @@
+# Homework 3 by N.Rudakov
+# Task 16
+
+import os
+import random
+
+os.system('cls')
+array_size = int(input())
+tested_array = [random.randint(1, 5) for _ in range(array_size)]
+print(tested_array)
