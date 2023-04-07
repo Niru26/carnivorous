e749a0ea9de67379766efecb8ed66e4173c160b2
Nikolay <nikolayrudakov@mail.ru>
2023-04-07 12:19:23 +0600

    Finished work on task 20 (3rd_prey) homework 3

---
+++ Compsognathus/3rd_prey.py
@@ -0,0 +1,34 @@
+# Homework 3 by N.Rudakov
+# Task 20
+
+import os
+os.system('cls')
+
+eng_dictionary = dict(A=1, E=1, I=1, O=1, U=1, L=1, N=1, S=1, T=1, R=1, D=2,
+                      G=2, B=3, C=3, M=3, P=3, F=4, H=4, V=4, W=4, Y=4, K=5, J=8, X=8, Q=10, Z=10)
+rus_dictionary = dict(А=1, В=1, Е=1, И=1, Н=1, О=1, Р=1, С=1, Т=1, Д=2, К=2, Л=2, М=2, П=2, У=2,
+                      Б=3, Г=3, Ё=3, Ь=3, Я=3, Й=4, Ы=4, Ж=5, З=5, Х=5, Ц=5, Ч=5, Ш=8, Э=8, Ю=8, Ф=10, Щ=10, Ъ=10)
+
+eng_rus_word = input("Enter English or Russian word: ")
+
+eng_rus_word_upper = eng_rus_word.upper()
+
+
+def count_points(dictionary_eng, dictionary_rus, word):
+    if word[0] in dictionary_eng:
+        dictionary = dictionary_eng
+    else:
+        dictionary = dictionary_rus
+    counter = 0
+    print("Each letter score: ", end='')
+    for key, point in dictionary.items():
+        for letter in word:
+            if letter == key:
+                counter += point
+                print(key, point, sep=' - ', end=', ')
+    return counter
+
+
+result = count_points(eng_dictionary, rus_dictionary, eng_rus_word_upper)
+print()
+print("Your total score:", result)
