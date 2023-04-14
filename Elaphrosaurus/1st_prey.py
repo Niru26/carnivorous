239bdd8c0ed46146d2b33de1928a38a2aa888b28
Nikolay <nikolayrudakov@mail.ru>
2023-04-14 10:31:36 +0600

    Finished work on task 26 (1st_prey), homework 5

--- Elaphrosaurus/1st_prey.py
+++ Elaphrosaurus/1st_prey.py
@@ -4,3 +4,17 @@
 # import random
 import os
 os.system('cls')
+
+number_A = int(input("Enter number A: "))
+number_B = int(input("Enter number B: "))
+
+
+def exponentiation(x, y):
+    if y == 0:
+        return 1
+
+    return exponentiation(x, y - 1) * x
+
+
+result = exponentiation(number_A, number_B)
+print(result)
